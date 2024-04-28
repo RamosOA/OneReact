b48891a70d69192d32f8c99028964abf9893ccf8
OscarRamos <oscara22a.r@gmail.com>
2024-04-28 00:43:00 -0500

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadFull } from "tsparticles";
+import logo from "./logo.svg";
+import "./App.css";
+import particlesOptions from "./particles.json";
+
+function App() {
+    const [init, setInit] = useState(false);
+
+    useEffect(() => {
+        if (init) {
+            return;
+        }
+
+        initParticlesEngine(async (engine) => {
+            await loadFull(engine);
+        }).then(() => {
+            setInit(true);
+        });
+    }, [init]);
+
+    return (
+        <div >
+            {init && <Particles options={particlesOptions} id="tsparticles"/>}
+                <header>
+                    <div className="logo">
+                        <div className="logo-nav">
+                            <a href="/" className="imagen"><img src={logo} alt="logo" /></a>
+                        </div>
+                        <nav className="navbar">
+                            <ul className="nav-links">
+                                <li><a href="/">Inicio</a></li>
+                                <li><a href="/about">Sobre nosotros</a></li>
+                                <li><a href="/events">Eventos</a></li>
+                                <li><a href="/contact">Contáctanos</a></li>
+                                <li><a href="/join">Únete</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </header>
+        </div>
+    );
+}
+
+export default App;
